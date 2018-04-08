ea5c3343b535f32af934bc3a325135220aad2f6d
Quintin <quint.oliver@gmail.com>
2018-04-08 14:40:39 -0400

    Added DroneGUI android app

---
+++ DroneGUI/app/src/main/java/com/teamprojects/team14/dronegui/MainActivity.java
@@ -0,0 +1,19 @@
+package com.teamprojects.team14.dronegui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Toast.makeText(this, "Picture taken", Toast.LENGTH_LONG).show();
+    }
+}
